eb7b8b0b5bdef4401d23398e40ac1c42e43a5354
mahardikaputra12 <mahardikaqohar@gmail.com>
2026-05-12 15:47:37 +0700

    pendefinisian prosedur pada class ibu diliuar class

--- ibu.h
+++ ibu.h
@@ -17,3 +17,17 @@ public:
     void tambahAnak(anak* pAnak);
     void cetakAnak();
 };
+void Ibu::tambahAnak(anak* pAnak) {
+    daftar_anak.push_back(pAnak);
+}
+
+void Ibu::cetakAnak() {
+    cout << "Daftar Anak dari Ibu \"" << this->nama << "\":\n";
+    for (auto& a : daftar_anak)
+    {
+        cout << a->nama << "\n";
+    }
+    cout << endl;
+}
+
+#endif
