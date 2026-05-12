3775a2913c60a22b5cd570d9a0b3b25b4fcd27ae
mahardikaputra12 <mahardikaqohar@gmail.com>
2026-05-12 15:31:06 +0700

    membuat construktor dan destructor pada class anak

--- anak.h
+++ anak.h
@@ -1,9 +1,17 @@
 #ifndef ANAK_H
 #define ANAK_H
 
-
 class anak {
 public:
+    string nama;
 
-   string nama;
-}
+    anak(string pNama) : nama(pNama) {
+        cout << "Anak \"" << nama << "\" ada \n";
+    }
+
+    ~anak() {
+        cout << "Anak \"" << nama << "\" tidak ada\n";
+    }
+};
+
+#endif
